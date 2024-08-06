e48419141195e12bb79688bf8294f5473bf394b9
hgupta110 <hgupta110@bloomberg.net>
2024-08-06 01:14:59 -0400

    added git ignore

--- out/build/cpp_basics/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/hgupta110/CPP_basics
-# Build directory: /Users/hgupta110/CPP_basics/out/build/cpp_basics
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("data_structures")
